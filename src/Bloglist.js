75c1c953c0bed9e393beddabf7805713bb89458d
nicxd531 <101049406+nicxd531@users.noreply.github.com>
2023-01-29 21:25:31 +0100

    updated read me

--- src/Bloglist.js
+++ src/Bloglist.js
@@ -11,13 +11,11 @@ const Bloglist = ({blogs,title, handleDelete}) => {
            {blogs && blogs.map((blogs)=> (
             <div className="blog-preview" key={blogs.id}>
                 <Link to={`/blogs/${blogs.id}`}>
-               
-                
                 <div>
-                <h2>{blogs.title}</h2>
-                <p><em>written by</em> {blogs.author}</p>
-                <p><em>Sub-category: </em>{blogs.blogCategory}</p>
-                </div>
+                    <h2>{blogs.title}</h2>
+                    <p><em>written by</em> {blogs.author}</p>
+                    <p><em>Sub-category: </em>{blogs.blogCategory}</p>
+                    </div>
                 </Link>
                 
             </div>))}
